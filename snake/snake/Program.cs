074785f83b77481e80a3d4039649dd8bf45dc744
dolzhenkovilya <chup87@mail.ru>
2021-03-30 12:36:43 +0300

    list added

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace snake
 {
@@ -6,8 +10,54 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Point p3 = new Point(5, 6, '!');
+            p3.Draw();
+
+            Point p4 = new Point(6, 4, '%');
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<char> charList = new List<char>();
+            charList.Add('!');
+            charList.Add('@');
+            charList.Add('%');
+
+          
+            foreach (int i in charList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+
             Console.ReadLine();
+
+
         }
+
     }
 }
