9dcecee80b24b0cc553ac536a3b043d82b47de05
dolzhenkovilya <chup87@mail.ru>
2021-03-30 18:22:35 +0300

    horizont

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,7 +6,20 @@ namespace snake
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Console.WriteLine("Hello World!");
+=======
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            HorizontLine line = new HorizontLine(5, 10, 8, '+');
+            line.Drow();
+
+
+>>>>>>> Stashed changes
             Console.ReadLine();
         }
     }
