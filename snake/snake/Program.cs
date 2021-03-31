41a0704fc6be8e28a8d0c0d441d4d0a034e444e1
dolzhenkovilya <chup87@mail.ru>
2021-03-31 13:55:24 +0300

    nasledovanie

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,21 +6,31 @@ namespace snake
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-            Console.WriteLine("Hello World!");
-=======
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Console.SetBufferSize(100, 35);
+            HorizontLine upLine = new HorizontLine(0, 78, 0, '+');
+            HorizontLine downLine = new HorizontLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontLine line = new HorizontLine(5, 10, 8, '+');
-            line.Drow();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+
+            //Point p1 = new Point(1, 3, '*');
+            //p1.Draw();
+
+            // Point p2 = new Point(4, 5, '#');
+            //p2.Draw();
+
+            //HorizontLine line = new HorizontLine(5, 10, 8, '+');
+            //line.Drow();
 
 
->>>>>>> Stashed changes
-            Console.ReadLine();
+            //Console.ReadLine();//
         }
     }
 }
