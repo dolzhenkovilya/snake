17e0c4fe4befd56f2d602ccb09f576d40d11f0fa
dolzhenkovilya <chup87@mail.ru>
2021-03-31 14:04:51 +0300

    new

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,8 +6,17 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            HorizontLine upLine = new HorizontLine(0, 78, 0, '+');
+            HorizontLine downLine = new HorizontLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+
         }
     }
 }
